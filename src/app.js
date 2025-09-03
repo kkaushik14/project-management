c9a0765f95c041abd71e226a1ee79ea47119e40d
kkaushik14 <kumarkaushik871@gmail.com>
2025-09-03 15:30:15 +0530

    feat: add health check endpoint and related routing/controller structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Express from "express";
 import cors from "cors";
+import healthCheckRouter from "./routes/healthcheck.routes.js";
 
 const app = Express();
 
@@ -7,19 +8,23 @@ app.use(Express.json({ limit: "16kb" }));
 app.use(Express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(Express.static("public"));
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:3278",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 app.get("/", (req, res) => {
-    res.send("I am in now...")
+  res.send("I am in now...");
 });
 
 app.get("/intro", (req, res) => {
-    res.send("Kaushik this side")
+  res.send("Kaushik this side");
 });
 
 export default app;
---
+++ src/controller/healthcheck.controller.js
@@ -0,0 +1,14 @@
+import { ApiResponse } from "../utils/api-response.js";
+
+const healthCheck = (req, res) => {
+  try {
+    res
+      .status(200)
+      .json(new ApiResponse(200, { message: "Server us running" }));
+  } catch (error) {
+    console.error("Connection failed", error);
+    process.exit(1);
+  }
+};
+
+export { healthCheck };
